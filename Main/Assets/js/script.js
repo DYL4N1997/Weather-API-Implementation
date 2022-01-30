c2bae056a68d732b8d1dce78ccb97c3bd05118bb
Dylan.Metcalf <dylan.metcalf15@gmail.com>
2022-01-30 22:52:04 +0000

    Push of more js changes

--- Main/Assets/js/script.js
+++ Main/Assets/js/script.js
@@ -1,7 +1,7 @@
 // City weather search and output section variables
 var weatherFormEl = $("#weatherform");
 var searchBtn = $("search");
-var cityOutput = $("cityoutput");
+var cityOutputEl = $("cityoutput");
 // Weather API key
 var API_Key = "4b542e0983727542e6d0ae70fafd5319";
 
@@ -14,20 +14,35 @@ function getCityByName(name) {
     });
 }
 
-function handleNoCityFound(title) {
+// No city found handler
+function handleNoCityFound(name) {
     var div = $(
       '<div class="alert alert-danger alert-dismissible fade show" role="alert"></div>'
     );
     var strong = $(
-      "<strong>We didn't find any citys with this name" + title + "</strong>"
+      "<strong>We didn't find any citys with this name" + name + "</strong>"
     );
     var button = $(
       '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
     );
-  
+    
+    div.append(strong);
+    div.append(button);
 }
 
+// Rendering of city name on submit
+function renderCityName(city) {
+    var name = city.Name
 
+    cityOutputEl.empty();
+
+    var cityNameBox = $(
+        '<div class="citynamebox" style="width: 100%; height: 35px; background-color: light-gray; color: black;>' + "<span>" + name + "</span>" + "</div>"
+    );
+
+    cityOutputEl.append(cityNameBox);
+
+}
 
 
 
