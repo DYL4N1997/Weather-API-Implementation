0b39695068089d590c54bc92e3647239934e4b65
Dylan.Metcalf <dylan.metcalf15@gmail.com>
2022-01-30 21:52:29 +0000

    Setting up fetch for by city name and no city found alert

--- Main/Assets/js/script.js
+++ Main/Assets/js/script.js
@@ -1,2 +1,43 @@
-var weatherAPIKey = "8a2c8931ac1f91b8693597dceedee189";
-var city;
+// City weather search and output section variables
+var weatherFormEl = $("#weatherform");
+var searchBtn = $("search");
+var cityOutput = $("cityoutput");
+// Weather API key
+var API_Key = "4b542e0983727542e6d0ae70fafd5319";
+
+function getCityByName(name) {
+    // api.openweathermap.org/data/2.5/weather?q=London,uk&appid=4b542e0983727542e6d0ae70fafd5319
+    return fetch(
+        "api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=" + API_Key
+    ).then(function (res) {
+        return res.json();
+    });
+}
+
+function handleNoCityFound(title) {
+    var div = $(
+      '<div class="alert alert-danger alert-dismissible fade show" role="alert"></div>'
+    );
+    var strong = $(
+      "<strong>We didn't find any citys with this name" + title + "</strong>"
+    );
+    var button = $(
+      '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+    );
+  
+}
+
+
+
+
+
+// Output to cards
+// var dayCardsEl = 
+ 
+// var London = 
+// var Brighton =
+// var Birmingham = 
+// var Manchester = 
+// var Plymouth = 
+
+
