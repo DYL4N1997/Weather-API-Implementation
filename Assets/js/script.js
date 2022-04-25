15ff71a6c10eb1cebe61b7b73652e6545d6e0b56
Dylan <user@users-MacBook-Pro-2.local>
2022-04-25 15:07:33 +0100

    Bulding out js fetch, create, ammend, & append

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -136,9 +136,19 @@ displayFutureWeather = (futureWeatherData) => {
             humidity: futureWeatherData[i].main.humidity,
         };
 
-        
-    }
-
+        let futureContainer = $(`
+        <div class="col-12 col-sm-12 col-lg-2 col-md-2 future-card-property">
+            <h5>${futureCityData.date}</h5>
+            <p>Max Temp:  ${futureCityData.maxTemp}  °C</p>
+            <p>Min Temp: ${futureCityData.minTemp}  °C</p>
+            <p>Wind: ${futureCityData.wind} MPH</p>
+            <p>Humidity: ${futureCityData.humidity} %</p>
+          <div>`);
+    
+        // append
+        $("#futureForecast").append(futureContainer);
+      }
+}
 
 cityFormEl.addEventListener("submit", searchLocation);
 
@@ -152,112 +162,3 @@ cityFormEl.addEventListener("submit", searchLocation);
 
 
 
-
-
-// var dayCards = document.querySelector("#day-cards");
-// var prevSearchBtn = document.querySelector("#previous-search");
-// var clearyBtn = document.querySelector("#cleary");
-// var cardReveal = document.querySelector(".card");
-// var hideContent = document.querySelector("#hidden");
-// var locBtn = new Object();
-// var displayContainer = document.querySelector("#display-container");
-// var todaysWeather = document.querySelector("todays-weather-container")
-
-
-
-
-// searchBtn.addEventListener("click", getCoords);
-
-
-// function getCoords (event) {
-//     event.preventDefault();
-//     var city = citySelectorEl.value.trim();
-//     if (city) {
-//         getCoords(city);
-//         citySelectorEl.textContent = " ";
-//         cityNameEl.textContent = " ";
-//         weatherOutputEl.textContent = " ";
-//         prevSearchBtn.textContent = " ";
-//         dayCards.textContent = " ";
-//     } else {
-//         alert ("You must enter a city");
-//     }
-// }
-
-// var getCoords = function (city) {
-//     var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+',*,&limit=1&appid='+API_KEY;
-//     fetch(apiUrl)
-//     .then(function (res) {
-//       if (res.ok) {
-//         res.json().then(function (data) {
-//           displayWeather(data);
-//       });
-//     }
-//   })
-// };
-
-// console.log("Cities Searched");
-
-// var displayWeather = function (possibleCities) {
-//   if (possibleCities.length===0) {
-//     var cityGroup=document.querySelector("#weather-output")
-//     cityGroup.textContent = "Please choose a valid city";
-//     return;
-//   }
-//   for (var i = 0; i < possibleCities.length; i++) {
-//     var city = possibleCities[i].name + '  ' + possibleCities[i].country;
-//     var cityCreate = document.createElement('ul');
-//     var citi = document.createElement('button');
-//     citi.classList = 'btn btn-success w-100 col-12 col-sm-12 col-md-12 col-lg-3'
-//     citi.textContent = city;
-//     citi.name = 'choice';
-//     citi.id = 'choice' +i;
-//     citi.value=i;
-//     displayContainer.appendChild(cityCreate);
-//     displayContainer.appendChild(citi);
-//   }
-
-// console.log ("Generated city buttons");
-
-// }
-
-// $("button[name='choice']").on('click',function() {
-//   var j=parseInt($(this).val());
-
-//   locBtn.citi=possibleCities[j].name;
-//   locBtn.lat=possibleCities[j].lat;
-//   locBtn.lon=possibleCities[j].lon;
-  
-//   myLocs.push({id:indexCities,locBtn});
-//   indexCities++;
-//   var index = myLocs.length- 1;
-//   previousCities(myLocs);
-//   getWeather(locBtn);}
-//   );
-
-// console.log ("LocBtn is saved")
-
-// function getWeather (locBtn) {
-//   var ocAPIURL = 'https//api.openweathermap.org/data/2.5/onecall?lat=' + locBtn.lat + '&lon=' + locBtn.lon + '&appid=' + API_KEY;
-//   fetch(ocAPIURL)
-//   .then(function (res) {
-//     if (res.ok) {
-//       res.json().then(function (data) {
-//         todaysWeather(data);
-
-//       });
-//     } else {
-//       res.ok=404 
-//       alert('Unable to display weather for this city');
-//     };
-// });
-
-// }
-
-
-
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-// ocApi = 'https//api.openweathermap.org/data/2.5/onecall?lat=' + locBtn.lat + '&lon=' + locBtn.lon + '&appid=' + API_KEY;
